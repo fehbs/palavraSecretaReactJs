c3679be69da6bb48c085ebefe312e4375e320cc5
fehbs <fernandobailao@gmail.com>
2022-06-23 12:39:12 -0300

    react-icons lib add

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -7,7 +7,7 @@ const StartScreen = ({startGame}) => {
     <div className='start'>
       <img src='./logo.png' className="logo" alt="logo" width="45%"/>
       <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para omeçar a jogar.</p>
+      <p>Clique no botão abaixo para começar a jogar</p>
       <button onClick={startGame}>Começar o jogo</button>
     </div>
   )
